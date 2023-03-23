2291baf6545f2cf7c833dbbf2a02673251acee91
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-23 15:44:59 -0300

    Requisito 1 faltando corrigir um teste

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -7,14 +7,23 @@ describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
     expect(typeof fetchProductsList).toBe('function')
   });
-  it('fetch é chamado ao executar fetchProductsList', () => {
 
+  it('fetch é chamado ao executar fetchProductsList', () => {
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
+  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    await expect(fetchProductsList('computador')).resolves.toBe(computadorSearch);
+  });
+
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: Termo de busca não informado.', async () => {
+    await expect(fetchProductsList()).rejects.toThrow('Termo de busca não informado')
+  });
+
 });
