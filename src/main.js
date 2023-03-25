c5deefef9d2a215b1fdf982327e4131dac5f8acb
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-25 10:08:24 -0300

    teste requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productSection = document.getElementsByClassName('products');
+
+const createList = () => {
+  const productList = fetchProductList('computador');
+  productList.forEach((product) => {
+    productSection.appendChild(createProductElement(product));
+  });
+};
+
+createList();
