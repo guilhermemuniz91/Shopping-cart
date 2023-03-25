566bca2d4bbd94760b0e125c4b8df4d26afa3f50
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-25 10:34:43 -0300

    correções e adições no requisito 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { searchCep } from './helpers/cepFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productSection = document.getElementsByClassName('products');
 
-const createList = () => {
-  const productList = fetchProductList('computador');
+const async createList = () => {
+  const productList = await fetchProductsList('computador');
   productList.forEach((product) => {
     productSection.appendChild(createProductElement(product));
   });
