2138ca234d28a96b5e26ffcf5f24ec929473d3b6
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-26 10:54:30 -0300

    tentativa requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,16 +1,18 @@
-export const getAddress = (cep) => {
+export const getAddress = async (cep) => {
   const firstAPI = `https://cep.awesomeapi.com.br/json/${cep}`;
   const secondAPI = `https://brasilapi.com.br/api/cep/v2/${cep}`;
-  const response1 = fetch(firstAPI);
-  const response2 = fetch(secondAPI);
-  const data1 = response1.json();
-  const data2 = response2.json();
+  const response1 = await fetch(firstAPI);
+  const response2 = await fetch(secondAPI);
+  const data1 = await response1.json();
+  const data2 = await response2.json();
   const promises = [data1, data2];
-  const whatComesFirst = promise.any(promises);
-return whatComesFirst;
+  const whatComesFirst = Promise.any(promises);
+  return whatComesFirst;
 };
 
-
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cepInput = document.getElementsByClassName('cep-input');
+  const spanAddress = document.getElementsByClassName('cart__address');
+  const clientAddress = await getAddress(cep);
+  spanAddress.innerText = `${whatComesFirst.address || whatComesFirst.street} - ${whatComesFirst.neighborhood || whatComesFirst.district} - ${whatComesFirst.city} - ${whatComesFirst.state}`;
 };
