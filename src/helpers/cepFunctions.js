3617b31dbec1329aff80517472cd5dccd60fc89f
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-03-27 11:09:22 -0300

    Requisito 11 concluído

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,22 +1,20 @@
 export const getAddress = async (cep) => {
   const firstAPI = `https://cep.awesomeapi.com.br/json/${cep}`;
   const secondAPI = `https://brasilapi.com.br/api/cep/v2/${cep}`;
-  const response1 = await fetch(firstAPI);
-  const response2 = await fetch(secondAPI);
-  const data1 = await response1.json();
-  const data2 = await response2.json();
-  const promises = [data1, data2];
-  const wcf = Promise.any(promises); // wcf stands for whatComesFirst
-  return wcf;
+  const promises = await Promise.any([fetch(firstAPI), fetch(secondAPI)]);
+  const response = await promises.json();
+  return response;
 };
 
-export const searchCep = async (cep) => {
-  // const cepInput = document.getElementsByClassName('cep-input');
+export const searchCep = async () => {
+  const cepInput = document.querySelector('.cep-input').value;
   const spanAddress = document.querySelector('.cart__address');
   try {
-    await getAddress(cep);
-    spanAddress.innerText = `${wcf.address || wcf.street} - ${wcf.neighborhood
-    || wcf.district} - ${wcf.city} - ${wcf.state}`;
+    const clientAddress = await getAddress(cepInput);
+    const { street, address, neighborhood, district, city, state } = clientAddress;
+    // spanAddress.innerText = `${address}, ${district}, ${city}, ${state}`;
+    spanAddress.innerText = `${address || street} - ${neighborhood
+      || district} - ${city} - ${state}`;
   } catch (Error) {
     spanAddress.innerText = 'CEP não encontrado';
   }
